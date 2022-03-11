3abeddf279a8c7aefd4212cdc4cf6f9034134966
Guta Neme <guta.neme@aau.edu.et>
2022-03-12 00:39:14 +0300

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,18 @@
-#!/bin/bash
+/*
+ *  * File: 4-puts.c
+ *   * Auth: Guta Neme
+ */
+
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *  * main - Prints "Programming is like building a multilingual
+ *   *                puzzle, followed by a new line.
+ *    *
+ *     * Return: Always 0.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
-
-
