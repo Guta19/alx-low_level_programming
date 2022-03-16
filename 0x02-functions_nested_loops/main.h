7245084e705b36b7a94ff7bcd13ffb6423721c57
Guta Neme <guta.neme@aau.edu.et>
2022-03-16 22:21:48 +0300

    add main to my repo

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,18 @@
-#ifndef main.h
-#define mian.h
-#void _putchar(s);
-#void print_alphabet();
+#ifndef MAIN_H
+#define MAIN_H
+void _putchar(char s);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int i);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int gc(int);
+int add(int, int);
+void print_to_98(int);
+void print_times_table(int);
+void multiple_35(void);
 #endif
