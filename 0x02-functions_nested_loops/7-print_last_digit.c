bba4cec3236d5b9748a547a98eada83712c7bcb8
Guta Neme <guta.neme@aau.edu.et>
2022-03-17 15:07:42 +0300

    add file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,14 @@
  *      *
  *       * Return: last digit
  */
-int print_last_digit(int n)
+int print_last_digit(int x)
 {
 	int last_digit = n % 10;
-	
+
 	if (last_digit < 0)
 		last_digit *= -1;
-	
+
 	_putchar(last_digit + '0');
-	
+
 	return (last_digit);
 }
