bb1c305d9da65479ed5ea1147d960424ed7511c4
Guta Neme <guta.neme@aau.edu.et>
2022-03-17 11:03:42 +0300

    add

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  * main -entry point
+ *   * Description: print alpahabet lower case
+ *    *
+ *     * Return: 0 always
+ */
+int main(void)
+{
+	print_alphabet();
+	
+	return (0);
+}
