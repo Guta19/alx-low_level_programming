4a5480c71323f94a689292e2ec6ecea9123e874e
Guta Neme <guta.neme@aau.edu.et>
2022-03-20 22:07:32 +0300

    add print diagonal file to my repo

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *  * print_diagonal - print symbol in diagonal
+ *   * @n: number of sybol to be printed
+ *    *
+ *     * Description: print symbol n times in diagonal
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
