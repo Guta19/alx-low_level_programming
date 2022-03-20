27f228920f5a1c7637dc46a5b991f0f87f1454d3
Guta Neme <guta.neme@aau.edu.et>
2022-03-20 21:54:08 +0300

    Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,62 @@
+#include "main.h"
+/**
+ *  * asci -convert to asci number
+ *   * @n: input digit
+ *    *
+ *     * Description: convert the digit to its asci code
+ *      *
+ *       * Return: the digits asci code
+ */
+int asci(int n)
+{
+	switch (n)
+	{
+		case 0:
+			return (48);
+		case 1:
+			return (49);
+		case 2:
+			return (50);
+		case 3:
+			return (51);
+		case 4:
+			return (52);
+		case 5:
+			return (53);
+		case 6:
+			return (54);
+		case 7:
+			return (55);
+		case 8:
+			return (56);
+		case 9:
+			return (57);
+		default:
+			return (0);
+	}
+}
+/**
+ *  * more_numbers -prin 10x a number
+ *   * Description: print multiple of 10 from 0 -14
+ */
+void more_numbers(void)
+{
+	int i;
+	int k;
+	
+	for (k = 0; k <= 9; k++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar(asci(i / 10));
+			}
+			_putchar(asci(i % 10));
+		}
+		_putchar('\n');
+	}
+}
+
+
+
