fd635c71fbe7336e45c645b73e1ccdfd39882e60
Guta Neme <guta.neme@aau.edu.et>
2022-03-20 19:09:16 +0300

    add main.c to my repo

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <limits.h>
+/**
+ *  * main - entry point
+ *   * Description: test function
+ *    *
+ *     * Return: 0 always
+ */
+int main(void)
+
+{
+	
+	print_number(INT_MIN);
+	_putchar('\n');
+	_putchar('\n');
+	return (0);
+}
