938c2f22ee00ea170ca48c1a661420bdf0707c40
Guta Neme <guta.neme@aau.edu.et>
2022-03-20 22:02:22 +0300

    add print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *  * print_line -draw straight line in terminal
+ *   * @n: number of times char drawn
+ *    *
+ *     * Description: draw a charactor c in terminal -straight line
+ */
+void print_line(int n)
+{
+	int i;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
