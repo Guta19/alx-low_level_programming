3033996fd9c80468fc0b3b2b358a011f16c1bb8c
Guta Neme <guta.neme@aau.edu.et>
2022-03-21 17:02:46 +0300

    add main file to my repo

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+void reset_to_98(int *n);
+void _putchar(char s);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+#endifi
