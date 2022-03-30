d2a4eb9812750e90015647ae424fbce157da6f73
Guta Neme <guta.neme@aau.edu.et>
2022-03-30 10:01:50 +0300

    update file 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,20 @@
 #include "main.h"
+
 /**
- *  * _memcpy -copy memory data
- *   * @dest: destination memory block
- *    * @src: source memory block
- *     * @n: number of bytes to copy
- *      *
- *       * Description: copy memory block of fdata from src to dest
- *        *
- *         * Return: pointer to destination
+ *  * _memcpy - Copies @n bytes from the memory area pointed
+ *   *           to by @src into that pointed to by @dest.
+ *    * @dest: A pointer to the memory area to copy @src into.
+ *     * @src: The source buffer to copy characters from.
+ *      * @n: The number of bytes to copy from @src.
+ *       *
+ *        * Return: A pointer to the destination buffer @dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-{
 		dest[i] = src[i];
-
 
 	return (dest);
 }
