60309b5c2af1473049a4a393f09022565df2b8b9
Guta Neme <guta.neme@aau.edu.et>
2022-03-30 09:57:39 +0300

    update file 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,14 +12,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char *d = dest;
-	char *s = src;
 
 	for (i = 0; i < n; i++)
-	{
-		d[i] = s[i];
-		i++;
-	}
+{
+		dest[i] = src[i];
+
 
 	return (dest);
 }
