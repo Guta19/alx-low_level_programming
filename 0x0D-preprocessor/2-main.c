fb2bd8e31bf6d69d7778b3f809398b77c46ba2d0
Guta19 <guta.neme@aau.edu.et>
2022-04-22 18:48:34 +0300

    update fir with new file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *  * main -entry point
+ *   *
+ *    * Description: main program
+ *     *
+ *      * Return: 0 always
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
