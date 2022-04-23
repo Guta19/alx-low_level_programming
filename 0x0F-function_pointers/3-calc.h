21f90019d225550658413735c5c5c0c665cd25ff
Guta19 <guta.neme@aau.edu.et>
2022-04-23 13:45:40 +0300

    add file

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,22 @@
+#ifndef CALCH
+#define CALCH
+
+/**
+ *  * struct op - structure for ops type
+ *   * @op: operatr
+ *    * @f: associated function
+ *     *
+ *      * Description:structure for operation
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int, int);
+} op_t;
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+#endif
