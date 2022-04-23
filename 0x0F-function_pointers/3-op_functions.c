35041f8fbc035f402813adf361df31dc75d8df7e
Guta19 <guta.neme@aau.edu.et>
2022-04-23 13:40:21 +0300

    add file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+/**
+ *  * op_add -add two nums
+ *   * @a: first
+ *    * @b: second
+ *     *
+ *      * Description: add two nums
+ *       *
+ *        * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *  * op_sub -subtract two nums
+ *   * @a: first num
+ *    * @b: second num
+ *     *
+ *      * Description: sub tract second num from first
+ *       *
+ *        * Return: diff
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *  * op_mul -multiply two nums
+ *   * @a: first num
+ *    * @b: second num
+ *     *
+ *      * Description: multiply two nums
+ *       *
+ *        * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *  * op_div -divide two nums
+ *   * @a: first
+ *    * @b: second
+ *     *
+ *      * Description: divide first num by second
+ *       *
+ *        * Return: div result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *  * op_mod -divide and return reminder
+ *   * @a: numerator
+ *    * @b: denom
+ *     *
+ *      * Description: return reminder
+ *       *
+ *        * Return: reminder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
